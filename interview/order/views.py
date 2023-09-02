a536d9056c85894dd4fff8ab34fdabfeadaeb745
omid zarinmahd <zarinpy@gmail.com>
2023-09-02 15:34:54 -0500

    Embargo date filters

--- interview/order/views.py
+++ interview/order/views.py
@@ -6,11 +6,26 @@ from rest_framework.views import APIView
 from interview.order.models import Order, OrderTag
 from interview.order.serializers import OrderSerializer, OrderTagSerializer, DeactivateOrderSerializer
 
+
 # Create your views here.
 class OrderListCreateView(generics.ListCreateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
-    
+
+    def list(self, request, *args, **kwargs):
+        start_embargo_date = kwargs.get("start_embargo_date", None)
+        end_embargo_date = kwargs.get("end_embargo_date", None)
+        queryset = self.get_queryset()
+        if start_embargo_date:
+            queryset = queryset.filter(embargo_date__gte=start_embargo_date)
+
+        if end_embargo_date:
+            queryset = queryset.filter(embargo_date__lte=start_embargo_date)
+
+        serializer = self.serializer_class(queryset, many=True)
+
+        return Response(serializer.data, 200)
+
 
 class OrderTagListCreateView(generics.ListCreateAPIView):
     queryset = OrderTag.objects.all()
